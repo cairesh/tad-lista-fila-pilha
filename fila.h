c8efa5f04393d5503db82ca7f4e6e4bab259f819
Samuel de Castro Matos <samuel.matos079@academico.ufgd.edu.br>
2026-09-09 16:35:29 -0400

    Implementação da Fila e estrutura inicial da mesma

--- fila.h
+++ fila.h
@@ -1,5 +1,15 @@
 #ifndef FILA_H
 #define FILA_H
 #define TAMANHO_FILA 5
-
+typedef struct {
+    int inicio;
+    int fim;
+    int tamanho;
+    int itens[TAMANHO_FILA];
+} fila;
+fila CriaFila(void);
+/* A lista que será criada será uma lista circular, deverá ser tratado erros tipo underflow 
+e underflow, também irá indicar se está vazia ou cheia, caso desejar adicionar outro elemento após 
+estar cheia perguntar se gostaria de substituir o primeiro elemento e tornar o segundo elemento o primeiro
+(fez sentido isso?)*/
 #endif
