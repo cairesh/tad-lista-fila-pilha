bff0e3aab690d738ca0d0202736ef7a45ac27c78
Samuel de Castro Matos <samuel.matos079@academico.ufgd.edu.br>
2026-09-10 19:38:23 -0400

    Implementação da fila

--- fila.c
+++ fila.c
@@ -1,8 +1,44 @@
 #include "fila.h"
-/* TODO: inicializar fila circular e controlar inicio/fim.
-   enqueue insere no final; dequeue remove do inicio (FIFO).
-   Tratar overflow e underflow; implementar vazia/cheia e exibicao.
-   Retornar a struct modificada por valor. Se necessario, usar registro
-   auxiliar para retornar tambem valor removido e estado. */
-/* Declaracao provisoria para manter o modulo C valido. */
-typedef int FilaImplementacaoPendente;
+fila cria_fila(){
+   fila Fila;
+   Fila.inicio=0;
+   Fila.fim=0;
+   Fila.tamanho=TAMANHO_FILA;
+   return Fila;
+}
+int verifica(fila Fila)
+{
+   int contador;
+   for (int i=0;i<TAMANHO_FILA;i++)
+   {
+      if (Fila.valores == NULL)
+         contador++;
+   }
+   if (contador == 0)
+      return 2;
+      /*verifica se todas as posições do vetor */
+   if (contador ==TAMANHO_FILA)
+      return 3;
+}
+int enqueue(fila Fila, int elem)
+{
+   int teste = verifica(Fila);
+   if (teste==2)
+   {
+      printf("Erro ao adicionar! A fila esta cheia\n");
+      return 1;
+   }
+   Fila.valores[Fila.fim] = elem;
+   Fila.fim = (Fila.fim+1)% TAMANHO_FILA;
+}
+int dequeue(fila Fila, int elem)
+{
+   int teste = verifica(Fila);
+   if (teste==3)
+   {
+      printf ("Erro ao remover! A fila esta vazia\n");
+      return 1;
+   }
+   Fila.valores[Fila.inicio]=NULL;
+   Fila.inicio= (Fila.inicio+1)% TAMANHO_FILA;   
+}
--- fila.h
+++ fila.h
@@ -1,5 +1,16 @@
 #ifndef FILA_H
+#include <stdio.h>
 #define FILA_H
-#define TAMANHO_FILA 5
-
+#define TAMANHO_FILA 6
+typedef struct {
+    int inicio;
+    int fim;
+    int valores[TAMANHO_FILA];
+    int tamanho;
+    
+} fila;
+fila CriaFila();
+int verifica(fila );
+int enqueue(fila, int);
+int dequeue(fila, int);
 #endif
