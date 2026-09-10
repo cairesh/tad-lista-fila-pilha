f741cba0d839618bc8d01c583519edb0cc0d529d
Arthurgoncalvesg <arthurgoncalvesg0212@gmail.com>
2026-09-10 19:08:50 -0400

    Update pilha.c
    minha parte

--- pilha.c
+++ pilha.c
@@ -1,8 +1,61 @@
+#include <stdio.h>
 #include "pilha.h"
-/* TODO: inicializar topo; implementar verificacoes de vazia/cheia.
-   push empilha; pop retira do topo (LIFO).
-   Tratar overflow e underflow; implementar exibicao.
-   Retornar a struct modificada por valor. Se necessario, usar registro
-   auxiliar para retornar tambem valor removido e estado. */
-/* Declaracao provisoria para manter o modulo C valido. */
-typedef int PilhaImplementacaoPendente;
+
+Pilha criarPilha(void) {
+    Pilha pilha;
+    int i;
+    pilha.topo = -1;
+    for (i = 0; i < TAMANHO_PILHA; i++) {
+        pilha.dados[i] = 0;
+    }
+    return pilha;
+}
+
+int pilhaVazia(Pilha pilha) {
+    return pilha.topo == -1;
+}
+
+int pilhaCheia(Pilha pilha) {
+    return pilha.topo == TAMANHO_PILHA - 1;
+}
+
+Pilha push(Pilha pilha, int valor) {
+    if (pilhaCheia(pilha)) {
+        printf("Erro: pilha cheia.\n");
+        return pilha;
+    }
+    pilha.topo++;
+    pilha.dados[pilha.topo] = valor;
+    return pilha;
+}
+
+Pilha pop(Pilha pilha) {
+    if (pilhaVazia(pilha)) {
+        printf("Erro: pilha vazia.\n");
+        return pilha;
+    }
+    pilha.dados[pilha.topo] = 0;
+    pilha.topo--;
+    return pilha;
+}
+
+int topoPilha(Pilha pilha) {
+    if (pilhaVazia(pilha)) {
+        printf("Erro: pilha vazia.\n");
+        return -1;
+    }
+    return pilha.dados[pilha.topo];
+}
+
+void exibirPilha(Pilha pilha) {
+    int i;
+    printf("Pilha: ");
+    if (pilhaVazia(pilha)) {
+        printf("vazia");
+    } else {
+        for (i = pilha.topo; i >= 0; i--) {
+            printf("%d ", pilha.dados[i]);
+        }
+    }
+    printf("\n");
+}
