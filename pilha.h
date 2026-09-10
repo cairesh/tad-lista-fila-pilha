6dbd4c18e0292accc42396754968c118d4f5fdbb
Arthurgoncalvesg <arthurgoncalvesg0212@gmail.com>
2026-09-10 19:09:46 -0400

    Update pilha.h
    minha parte

--- pilha.h
+++ pilha.h
@@ -1,7 +1,17 @@
 #ifndef PILHA_H
 #define PILHA_H
 #define TAMANHO_PILHA 5
-/* TODO: definir registro Pilha com vetor e indice topo.
-   Declarar criarPilha, push, pop, pilhaVazia, pilhaCheia e exibirPilha.
-   Receber e retornar structs por valor, sem ponteiros. */
+
+typedef struct {
+    int dados[TAMANHO_PILHA];
+    int topo;
+} Pilha;
+
+Pilha criarPilha(void);
+Pilha push(Pilha pilha, int valor);
+Pilha pop(Pilha pilha);
+int pilhaVazia(Pilha pilha);
+int pilhaCheia(Pilha pilha);
+void exibirPilha(Pilha pilha);
+
 #endif
